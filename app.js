42f69bc2b000033ea37af0a07fbeb6b8cc020f3e
Abdul qadeer <102538683+qadeer77@users.noreply.github.com>
2022-08-01 15:51:58 +0500

    design-redy

---
+++ app.js
@@ -0,0 +1,6 @@
+function player1() {
+    swal("Player 1 Keys!", "Forward = Right Key \n Back = Left Key \n Jump = Up Key \n Kick = Down Key \n Mukka = O");
+}
+function player2() {
+    swal("Player 2 Keys!", "Forward = A \n Back = Z \n Jump = J \n Kick = K \n Mukka = B");
+}
